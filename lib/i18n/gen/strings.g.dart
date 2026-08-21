70565aa8ebeb2633ee441b08cf8cbef23f57f8c5
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-08-22 01:36:56 +0900

    feat: added speedup anntena

--- lib/i18n/gen/strings.g.dart
+++ lib/i18n/gen/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 1
-/// Strings: 231
+/// Strings: 237
 ///
-/// Built on 2026-08-21 at 16:22 UTC
+/// Built on 2026-08-21 at 16:36 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
