89d809acd898927cc300d9804ed4de910a483169
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-08-22 00:16:14 +0900

    feat: added head shape: onigiri

--- lib/i18n/gen/strings.g.dart
+++ lib/i18n/gen/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 1
-/// Strings: 227
+/// Strings: 228
 ///
-/// Built on 2026-08-21 at 14:57 UTC
+/// Built on 2026-08-21 at 15:15 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
