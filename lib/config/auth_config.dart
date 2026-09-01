ac9bd164a782a2ec4e6bd5526f8f55422113b16c
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-09-01 21:08:55 +0900

    fix: Correct android debug endpoint URL format in AuthApiConfig

--- lib/config/auth_config.dart
+++ lib/config/auth_config.dart
@@ -11,8 +11,8 @@ import 'package:flutter/foundation.dart' show kDebugMode;
 class AuthApiConfig {
   const AuthApiConfig();
 
-  static const _productionEndpoint = 'https://api.denpa_memo.karasu256.com';
-  static const _androidDebugEndpoint = 'https://denpamemo_auth_dev.karasu256.com';
+  static const _productionEndpoint = 'https://api.denpamemo.karasu256.com';
+  static const _androidDebugEndpoint = 'https://denpamemo-auth-dev.karasu256.com';
 
   String get baseUrl {
     if (!kDebugMode) {
